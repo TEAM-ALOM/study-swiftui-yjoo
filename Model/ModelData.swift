506fc067d46c3c6071d849de41354b437db4147a
Y-Joo <wldudwn99@gmail.com>
2023-10-11 23:13:10 +0900

    Feat: Paused Handling user input due to version issue

--- Model/ModelData.swift
+++ Model/ModelData.swift
@@ -7,6 +7,10 @@
 
 import Foundation
 
+class ModelData: ObservableObject{
+    @Published var landmarks: [Landmark] = load("landmarkData.json")
+}
+
 var landmarks: [Landmark] = load("landmarkData.json")
 
 func load<T: Decodable>(_ filename: String) -> T {
--- Views/LandmarkRow.swift
+++ Views/LandmarkRow.swift
@@ -17,6 +17,10 @@ struct LandmarkRow: View {
                 .frame(width: 50, height: 50)
             Text(landmark.name)
             Spacer()
+            if landmark.isFavorite {
+                Image(systemName:"star.fill")
+                    .foregroundStyle(.yellow)
+            }
         }
     }
 }
@@ -24,9 +28,9 @@ struct LandmarkRow: View {
 struct LandmarkRow_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: ModelData().landmarks[0])
                 .previewDisplayName("Turtle Rock")
-            LandmarkRow(landmark: landmarks[1])
+            LandmarkRow(landmark: ModelData().landmarks[1])
                 .previewDisplayName("Salmon")
         }
     }
